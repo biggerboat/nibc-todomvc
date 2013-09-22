85370e5a5b77815ebbfe54710b1eb6a04707e735
Paul Tondeur <paul@paultondeur.nl>
2013-09-22 17:44:15 +0200

    force imports for tweenmax plugins

--- public/js/app/common.js
+++ public/js/app/common.js
@@ -1,68 +1,70 @@
 requirejs.config({
-	paths: {
-		"vendors": "../vendors",
-		"text": "../vendors/requirejs-text/text",
-		"jquery": "../vendors/jquery/jquery",
-		"handlebars": "../vendors/handlebars/handlebars",
-		"underscore": "../vendors/underscore/underscore",
-		"backbone": "../vendors/backbone/backbone",
-		"injector-js": "../vendors/injector.js/injector-js",
-		"navigator-js": "../vendors/navigator.js/navigator-js",
-		"backbone-super": "../vendors/backbone-super/backbone-super/backbone-super",
-		"backbone-command": "../vendors/backbone-command/backbone-command",
-		"backbone-injector": "../vendors/backbone-injector/backbone-injector",
-		"TweenMax": "../vendors/greensock-js/src/uncompressed/TweenMax",
-		"templates": "../templates"
+	paths:{
+		"vendors":"../vendors",
+		"text":"../vendors/requirejs-text/text",
+		"jquery":"../vendors/jquery/jquery",
+		"handlebars":"../vendors/handlebars/handlebars",
+		"underscore":"../vendors/underscore/underscore",
+		"backbone":"../vendors/backbone/backbone",
+		"injector-js":"../vendors/injector.js/injector-js",
+		"navigator-js":"../vendors/navigator.js/navigator-js",
+		"backbone-super":"../vendors/backbone-super/backbone-super/backbone-super",
+		"backbone-command":"../vendors/backbone-command/backbone-command",
+		"backbone-injector":"../vendors/backbone-injector/backbone-injector",
+		"TweenMax":"../vendors/greensock-js/src/uncompressed/TweenMax",
+		"TweenEasePack":"../vendors/greensock-js/src/uncompressed/easing/EasePack",
+		"TweenCSSPlugin":"../vendors/greensock-js/src/uncompressed/plugins/CSSPlugin",
+		"templates":"../templates"
 	},
 
 	// Sets the configuration for your third party scripts that are not AMD compatible
-	shim: {
+	shim:{
 
-		"handlebars": {
-			"exports": "Handlebars"
+		"handlebars":{
+			"exports":"Handlebars"
 		},
 
-		"injector-js": {
-			"exports": "injector.Injector"
+		"injector-js":{
+			"exports":"injector.Injector"
 		},
 
-		"navigator-js": {
-			"deps": ["jquery"],
-			"exports": "navigatorjs.Navigator"
+		"navigator-js":{
+			"deps":["jquery"],
+			"exports":"navigatorjs.Navigator"
 		},
 
-		"underscore": {
-			"exports": "_"
+		"underscore":{
+			"exports":"_"
 		},
 
-		"backbone": {
-			"deps": ["underscore","jquery"],
-			"exports": "Backbone"  //attaches "Backbone" to the window object
+		"backbone":{
+			"deps":["underscore", "jquery"],
+			"exports":"Backbone"  //attaches "Backbone" to the window object
 		},
 
-		"backbone-super": {
-			"deps": ["backbone"]
+		"backbone-super":{
+			"deps":["backbone"]
 		},
 
-		"backbone-command": {
-			"deps": ["backbone"],
-			exports: "Backbone.Command"
+		"backbone-command":{
+			"deps":["backbone"],
+			exports:"Backbone.Command"
 		},
 
-		"backbone-injector": {
-			"deps": ["injector-js","backbone"]
+		"backbone-injector":{
+			"deps":["injector-js", "backbone"]
 		},
 
-		"TweenMax": {
-			"exports": "TweenMax"
+		"TweenMax":{
+			"exports":"TweenMax"
 		},
 
-		"vendors/greensock-js/src/uncompressed/plugins/CSSPlugin": {
-			"deps": ["TweenMax"]
+		"TweenEasePack":{
+			"deps":["TweenMax"]
 		},
 
-		"vendors/greensock-js/src/uncompressed/easing/EasePack": {
-			"deps": ["TweenMax"]
+		"TweenCSSPlugin":{
+			"deps":["TweenMax"]
 		}
 	}
 });
@@ -77,9 +79,9 @@ define([
 	"backbone-super",
 	"backbone-command",
 	"backbone-injector",
-	"TweenMax"
-], function () {
-
+	"TweenMax",
+	"TweenEasePack",
+	"TweenCSSPlugin"
+], function() {
 	//Enforce loading globally used libraries
-
 });

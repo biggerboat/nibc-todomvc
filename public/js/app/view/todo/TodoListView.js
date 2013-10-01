5edb7b27b52835db5bfea03780fa1bf38d280a43
Paul Tondeur <paul@paultondeur.nl>
2013-10-01 11:36:18 +0200

    Checking off todo items

--- public/js/app/view/todo/TodoListView.js
+++ public/js/app/view/todo/TodoListView.js
@@ -10,12 +10,15 @@ define([
 		tagName: 'section',
 		template: Handlebars.compile(template),
 
+		todoCollection: 'inject',
 		todosModel: 'inject',
 
 		events: {
+			'click .toggle': 'onToggleComplete'
 		},
 
 		initialize: function() {
+			this.listenTo(this.todoCollection, "change", this.render);
 			this.listenTo(this.todosModel, "change:filteredTodos", this.render);
 
 			this.render();
@@ -23,7 +26,19 @@ define([
 
 		render: function() {
 			this.$el.html(this.template(this.todosModel.toJSON()));
+
 			return this;
+		},
+
+		onToggleComplete: function(e) {
+			var $checkbox = $(e.target),
+				isCompleted = $checkbox.is(':checked'),
+				$todoItem = $checkbox.parents(".todo:first"),
+				todoIndex = $todoItem.data('index'),
+				filteredTodos = this.todosModel.get('filteredTodos'),
+				todoItemModel = filteredTodos[todoIndex];
+			
+			todoItemModel.set({completed:isCompleted})
 		}
 	});
 
